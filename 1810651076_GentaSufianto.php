50b4c8b19af569e3afe28ec4d05b6a0679aaeebb
GentaS <61726195+GentaS@users.noreply.github.com>
2020-05-12 14:27:48 +0700

    Pertemuan 2

---
+++ 1810651076_GentaSufianto.php
@@ -0,0 +1,113 @@
+<title>Pemrograman Berbasis Web</title>
+<head>
+<p style="text-align: center; font-size: 50px;">Pemrograman Berbasis Web</p>
+<?php 
+	$tes = "Saya Genta Sufianto"; 
+	print 'Berikut adalah type data string : '.$tes;
+?>
+<html>
+<head>
+ <title>belajar membuat paragraf</title>
+</head>
+<body>
+ <p align="left">1. ini merupakan penulisan paragraf rata kiri dalam dokument HTML</p>
+ <p align="right">2. ini merupakan penulisan paragraf rata kanan dalam dokument HTML</p>
+ <p align="center">3. ini merupakan penulisan paragraf rata tengah dalam dokument HTML</p>
+ <p align="justivy">4. ini merupakan penulisan paragraf rata kiri kanan dalam dokument HTML</p>
+</body>
+</html>
+
+
+<html>
+<head>
+	<title>Menghubungkan HTML dengan CSS </title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Belajar HTML menghubungkan html dengan css</h1>
+</body>
+</html>
+
+<html>
+<head>
+	<title>belajar membuat tabel di html</title>
+</head>
+<body>
+<table border="1">
+	<tr>
+		<th>No</th>
+		<th>Nama</th>
+		<th>Alamat</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Genta</td>
+		<td>Surabaya</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Derryl</td>
+		<td>Jember</td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td>Alif</td>
+		<td>Bondowoso</td>
+	</tr>
+	
+<html>
+</html>
+</table>
+<head>
+	<title>membuat hyperlink</title>
+</head>
+
+<body>
+ini adalah contoh alamat Profile <a href="https://www.facebook.com/gentasufianto">klik disini</a>
+</body>
+
+
+<html>
+<head>
+	<title>belajar membuat list</title>
+</head>
+<body>
+<ul>
+	<li>Sinau HTML</li>
+	<li>Sinau PHP</li>	
+</ul>
+</body>
+</html>
+
+<html>
+<head>
+	<title>Belajar Atribut Form pada html</title>
+</head>
+<body>
+<form>
+	Nama Depan :<input type="text" value="Jenengmu" maxlength="10"><br/>
+	Nama Belakang : <input type="text">
+</form>
+ 
+</body>
+</html>
+
+
+<html>
+<head>
+	<title>Mengenal Class dan Id pada HTML | www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<h1>Mengenal Class dan Id pada HTML
+	<!-- contoh penggunaan class -->
+	<div class="Panggil">ID 1</div>
+	<div class="Panggil">ID 2</div>
+	<div class="Panggil">ID 3</div>
+ 
+	<!-- contoh penggunaan id -->
+	<div id="Panggil">ID 4</div>
+</body>
+</html>
+</p>
+</body>
